55bac1de4aeb602dd4a55585a13543e6a86715a5
Anastasiya Tiunova <a_tiunova99@mail.ru>
2020-09-17 12:54:40 +0300

    Decorator +tests

--- src/test/java/com/acme/edu/DecoratorTest.java
+++ src/test/java/com/acme/edu/DecoratorTest.java
@@ -1,4 +1,35 @@
 package com.acme.edu;
 
-public class DecoratorTest {
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DecoratorTest implements SysoutCaptureAndAssertionAbility{
+    private Decorator sut;
+
+    @Before
+    public void setUp(){
+        sut = new Decorator();
+        resetOut();
+        captureSysout();
+    }
+
+    @Test
+    public void outShouldContainsMessage(){
+        String message= "test";
+        System.out.println(sut.decorate(message));
+
+        assertSysoutContains(message);
+    }
+    @Test
+    public void outShouldContainsCorrectDate(){
+        String message= "test";
+        System.out.println(sut.decorate(message));
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+
+        assertSysoutContains(formatter.format(date));
+    }
 }
