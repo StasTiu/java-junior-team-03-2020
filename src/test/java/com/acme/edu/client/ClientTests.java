07315f35c09a4a4f5934de4b629d2e7c72447667
evgeniask <evisunny@yandex.ru>
2020-09-17 11:59:45 +0300

    Test templates added

---
+++ src/test/java/com/acme/edu/client/ClientTests.java
@@ -0,0 +1,13 @@
+package java.com.acme.edu.client;
+import org.junit.Before;
+
+public class ClientTests {
+
+    private com.acme.edu.client.Client sut;
+
+    @Before
+    public void setUp() {
+    }
+
+
+}
---
+++ src/test/java/com/acme/edu/server/ServerTests.java
@@ -0,0 +1,13 @@
+package java.com.acme.edu.server;
+import org.junit.Before;
+
+public class ServerTests {
+
+    private com.acme.edu.client.Client sut;
+
+    @Before
+    public void setUp() {
+    }
+
+
+}
