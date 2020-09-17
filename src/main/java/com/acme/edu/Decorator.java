824ad3073238736e278cbbe905d925435124b701
Anastasiya Tiunova <a_tiunova99@mail.ru>
2020-09-17 12:10:56 +0300

    decorator change

--- src/main/java/com/acme/edu/Decorator.java
+++ src/main/java/com/acme/edu/Decorator.java
@@ -1,7 +1,13 @@
 package com.acme.edu;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Decorator {
     public String decorate(String message){
-        return message;
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+//        System.out.println(formatter.format(date));
+        return message + formatter.format(date);
     }
 }
