380926985465ad60d001c464b50b04be0fb0fefd
dfrant <defrant98@mail.ru>
2020-09-17 12:02:53 +0300

    Client - Server classes

--- src/main/java/com/acme/edu/client/Client.java
+++ src/main/java/com/acme/edu/client/Client.java
@@ -1,10 +1,13 @@
 package com.acme.edu.client;
 
+import com.acme.edu.Printer;
+
 import java.io.*;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
+        Printer printer = new Printer();
         try (final
              Socket connection = new Socket("127.0.0.1", 10_000);
              final DataInputStream input = new DataInputStream(
@@ -14,8 +17,9 @@ public class Client {
                      new BufferedOutputStream(
                              connection.getOutputStream()));
         ) {
-
-
+            out.writeUTF();
+            out.flush();
+            printer.print(input.readUTF());
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/com/acme/edu/server/ServerRunner.java
@@ -0,0 +1,30 @@
+package com.acme.edu.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerRunner {
+
+    public static void main(String[] args) {
+        Decorator decorator = new Decorator();
+        String message = "";
+        try (final ServerSocket connectionPortListener = new ServerSocket(10_000);
+             final Socket clientConnection = connectionPortListener.accept();
+             final DataInputStream input = new DataInputStream(
+                     new BufferedInputStream(
+                             clientConnection.getInputStream()));
+             final DataOutputStream out = new DataOutputStream(
+                     new BufferedOutputStream(
+                             clientConnection.getOutputStream()))) {
+
+
+            message = input.readUTF();
+            message = decorator.decorate(message);
+            out.writeUTF(message);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
