ae40852e7a1c68b3a55b681c585c9481bf564875
dfrant <defrant98@mail.ru>
2020-09-18 16:06:10 +0300

    Refactoring

--- src/main/java/com/acme/edu/server/ServerRunner.java
+++ src/main/java/com/acme/edu/server/ServerRunner.java
@@ -2,7 +2,7 @@ package com.acme.edu.server;
 
 import com.acme.edu.ConfigReader;
 
-import java.io.*;
+import java.io.IOException;
 
 public class ServerRunner {
 
@@ -11,32 +11,4 @@ public class ServerRunner {
         Server server  = new Server(properties.getPort());
         server.startServer();
     }
-    /*
-    public static void main(String[] args) {
-        Decorator decorator = new Decorator();
-        FileSaver saver = new FileSaver("history.txt");
-        String message = "";
-        ConfigReader properties = new ConfigReader();
-
-        try (final ServerSocket connectionPortListener = new ServerSocket(properties.getPort());
-             final Socket clientConnection = connectionPortListener.accept();
-             final DataInputStream input = new DataInputStream(
-                     new BufferedInputStream(
-                             clientConnection.getInputStream()));
-             final DataOutputStream out = new DataOutputStream(
-                     new BufferedOutputStream(
-                             clientConnection.getOutputStream()))) {
-
-
-            while (true) {
-                message = input.readUTF();
-                message = decorator.decorate(message);
-                saver.save(message);
-                out.writeUTF(message);
-                out.flush();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
 }
